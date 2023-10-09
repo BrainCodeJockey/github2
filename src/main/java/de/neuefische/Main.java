4714273e576c9f1d63b39dc882a35395376c9539
Alex <alexander.unmuth@gmail.com>
2023-10-09 11:08:45 +0200

    feature_createPlint

--- src/main/java/de/neuefische/Main.java
+++ src/main/java/de/neuefische/Main.java
@@ -3,5 +3,10 @@ package de.neuefische;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Feature 2");
+
+
     }
 }
